19b4828b7c61c46d4bc5bda4308eaa6fe5108e13
unknown <felixtolawinjobi@gmail.com>
2022-11-09 06:16:50 -0500

    second commit for crud

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=400)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artist_id = models.PositiveIntegerField()
+
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=4000)
+    song_id = models.PositiveIntegerField()
